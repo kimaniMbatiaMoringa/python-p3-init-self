6c097c2e5bbc7ffd1fa09420f6a61b724403ece7
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-08-28 12:38:09 +0300

    Assignment completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def __init__(self, name, breed = "Mutt"):         #Runs whenever a instance of the class is created
+        self.name = name
+        self.breed = breed
+        #print(f"{name} is born!")
     pass
+
+
+
+    #def bark(self):
+    #    print("Woof!")
+
+    #def adopt(self,owner_name):
+    #    self.owner = owner_name    
+
+#fido = Dog("Fido")        
